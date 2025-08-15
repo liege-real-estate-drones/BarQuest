5fb999033225081e000ef510a90c7fc332769d08
Thibault Wislez <thibault1wislez@gmail.com>
2025-08-15 22:44:26 +0000

    Les compétences sont toujours disposées de façon à recouvrir les statist

--- src/features/combat/CombatView.tsx
+++ src/features/combat/CombatView.tsx
@@ -91,7 +91,7 @@ export function CombatView() {
       </header>
 
       <main className="flex-grow grid grid-cols-1 md:grid-cols-2 gap-4 min-h-0">
-          <div className="grid grid-rows-2 gap-4">
+          <div className="flex flex-col gap-4">
             <EntityDisplay entity={player} isPlayer />
             <EntityDisplay entity={enemy} />
           </div>
